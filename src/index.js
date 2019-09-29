62b2d4a857884bd9c2ea02d163ee3fe811be3d75
Cat Core <arthur.koalas@free.fr>
2019-09-29 15:02:50 +0200

    random push lol

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Main from "./components/main";
+import Header from "./components/header";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<Main />, document.getElementById("root"));
+ReactDOM.render(<Header />, document.getElementById("headerroot"));
+ReactDOM.render(<Main />, document.getElementById("bodyroot"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
