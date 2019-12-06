972fac70ee83fe1b07114667e3aa52094c997726
OliviaTheVampire <sindrefagerheim53@gmail.com>
2019-12-06 20:02:49 +0100

    more work on dark mode

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-function toggleDarkMode() {
-	$(".app-container").toggleClass("dark");
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
 
-	if ($(".app-container").hasClass("dark")) {
-		$("#darkMode").html("check_box");
-		$("#darkMode")
-			.parent()
-			.addClass("active");
-	} else {
-		$("#darkMode").html("check_box_outline_blank");
-		$("#darkMode")
-			.parent()
-			.removeClass("active");
-	}
+if (currentTheme) {
+    document.documentElement.setAttribute('theme', currentTheme);
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
 }
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {        document.documentElement.setAttribute('theme', 'light');
+          localStorage.setItem('theme', 'light');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
